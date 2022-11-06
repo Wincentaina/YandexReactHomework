4e661e90e000181001bf6dfc9fd302aa25f4b8ea
Wincentaina <103582986+Wincentaina@users.noreply.github.com>
2022-11-06 12:28:40 +0700

    finale

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Layout from "./components/Layout/Layout";
 import {books} from "./constants/mock";
-// import StorePage from "./pages/StorePage/StorePage";
-import InfoPage from "./pages/InfoPage/InfoPage";
+import StorePage from "./pages/StorePage/StorePage";
+// import InfoPage from "./pages/InfoPage/InfoPage";
 
 function App() {
     return (
         <Layout>
-            {/*<StorePage books={books}/>*/}
-            <InfoPage book={books[0]}/>
+            <StorePage books={books}/>
+            {/*<InfoPage book={books[0]}/>*/}
         </Layout>
     );
 }
